3ca3ff29e271a7f038d721c2a8c189c40ea3a473
Arthur <servinarthur@yahoo.com>
2018-07-02 20:38:09 -0700

    finished day 1 in class work

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//DECLARE A VARIABLE
+var firstName;
+
+//DEFINE YOUR VARIABLE
+firstName = "Arthur" //String
+
+//DEFINE NAD DECLARE YOUR VARIABLE
+var lastName = "Servin"
+
+console.log(firstName, lastName);
+
+firstName = "Ross"
+
+console.log(firstName);
