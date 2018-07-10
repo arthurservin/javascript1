4d86fb5334d488ac68750768ad3e32f1c27f4c05
Arthur <servinarthur@yahoo.com>
2018-07-09 21:19:30 -0700

    takehome 1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//Day1 Takehome
+
+var myAnswer1 = "I am looking to get a working grasp on a language that can be applicble to many ideas in the future.  I also had previous code written for me that I hope I can understand a little better.";
+
+console.log (myAnswer1);
+
+var myAnswer2 = "I have a passion for music so when I get the time I like to play guitar and sing a little karaoke."
+
+console.log (myAnswer2);
