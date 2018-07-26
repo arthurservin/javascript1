8d00e518192e904db15dccf90b3aa553ec3efa22
Arthur <servinarthur@yahoo.com>
2018-07-25 21:00:23 -0700

    day 8 start

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let loginForm = document.forms.login;
+
+let emailInput = loginForm.email;
+
+/////  everything after question mark (?) is a query of the DOM to the backend
+
+//// %40 = @
+
+//// ampersand (&) seperates perameters
+
+/// in order to talk to a server there are 2 verbs = get or post
+//  get = you are not sending any parameters, sending sending to url and getting something back (default method)
+//  post = when you need to login with parameters
+
+
+
+
+
+
+
+
+
+
+
+
+
+////
